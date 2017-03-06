cc0b7aff112646aaff59f0841ee41c0c4a6382d9
Anirudh S <anirudh24seven@gmail.com>
2017-03-06 10:33:01 +0530

    Fix typos in acct.c

--- kernel/acct.c
+++ kernel/acct.c
@@ -24,8 +24,8 @@
  *  Now we silently close acct_file on attempt to reopen. Cleaned sys_acct().
  *  XTerms and EMACS are manifestations of pure evil. 21/10/98, AV.
  *
- *  Fixed a nasty interaction with with sys_umount(). If the accointing
- *  was suspeneded we failed to stop it on umount(). Messy.
+ *  Fixed a nasty interaction with with sys_umount(). If the accounting
+ *  was suspended we failed to stop it on umount(). Messy.
  *  Another one: remount to readonly didn't stop accounting.
  *	Question: what should we do if we have CAP_SYS_ADMIN but not
  *  CAP_SYS_PACCT? Current code does the following: umount returns -EBUSY
